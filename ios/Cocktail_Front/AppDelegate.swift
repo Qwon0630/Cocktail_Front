750745fe9a3ff556d424db80e786bbedcfd16069
Qwon0630 <81517768+Qwon0630@users.noreply.github.com>
2025-03-12 22:59:49 +0900

    네이버 로그인 ios 설정

--- ios/Cocktail_Front/AppDelegate.swift
+++ ios/Cocktail_Front/AppDelegate.swift
@@ -3,13 +3,19 @@ import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 import GoogleMaps
+import NaverThirdPartyLogin
+
 @main
 class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
     self.moduleName = "Cocktail_Front"
     self.dependencyProvider = RCTAppDependencyProvider()
-
+    
     GMSServices.provideAPIKey("AIzaSyDeVQ2wHr3QIavBa4RGYwZly8h0sNTfLmQ")
+
     // You can add your custom initial props in the dictionary below.
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
@@ -23,9 +29,21 @@ class AppDelegate: RCTAppDelegate {
 
   override func bundleURL() -> URL? {
 #if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
+  }
+
+  // 네이버 로그인 URL 핸들링
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    if NaverThirdPartyLoginConnection.getSharedInstance()?.application(app, open: url, options: options) == true {
+      return true
+    }
+    return super.application(app, open: url, options: options)
   }
 }
--- src/Screens/Login.tsx
+++ src/Screens/Login.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect,useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { StackScreenProps } from "@react-navigation/stack";
-// import NaverLogin from "@react-native-seoul/naver-login";
-// import type {
-//   GetProfileResponse,
-//   NaverLoginResponse,
-// } from "@react-native-seoul/naver-login";
+import NaverLogin from "@react-native-seoul/naver-login";
+import type {
+  GetProfileResponse,
+  NaverLoginResponse,
+} from "@react-native-seoul/naver-login";
 import  * as KakaoLogin from "@react-native-seoul/kakao-login";
  
   const consumerKey = 'pjSp0u95mvc6Ufq_TQbP';
@@ -23,36 +23,34 @@ type LoginScreenProps = StackScreenProps<RootStackParamList, "Login">;
 
 const LoginScreen: React.FC<LoginScreenProps> = ({navigation}) => {
 
-  // useEffect(() => {
-  //   NaverLogin.initialize({
-  //     appName,
-  //     consumerKey,
-  //     consumerSecret,
-  //     serviceUrlSchemeIOS: serviceUrlScheme,
-  //     disableNaverAppAuthIOS: true,
-  //   });
-  // }, []);
+  useEffect(() => {
+    NaverLogin.initialize({
+      appName,
+      consumerKey,
+      consumerSecret,
+      serviceUrlSchemeIOS: serviceUrlScheme,
+      disableNaverAppAuthIOS: true,
+    });
+  }, []);
 
-  // const [success, setSuccessResponse] =
-  //   useState<NaverLoginResponse['successResponse']>();
+  const [success, setSuccessResponse] = useState<NaverLoginResponse['successResponse']>();
 
-  // const [failure, setFailureResponse] =
-  //   useState<NaverLoginResponse['failureResponse']>();
-  // const [getProfileRes, setGetProfileRes] = useState<GetProfileResponse>();
+  const [failure, setFailureResponse] = useState<NaverLoginResponse['failureResponse']>();
+  const [getProfileRes, setGetProfileRes] = useState<GetProfileResponse>();
 
-  // const naverLogin = async (): Promise<void> => {
-  //   const { failureResponse, successResponse } = await NaverLogin.login();
-  //   setSuccessResponse(successResponse);
-  //   setFailureResponse(failureResponse);
-  // };
+  const naverLogin = async (): Promise<void> => {
+    const { failureResponse, successResponse } = await NaverLogin.login();
+    setSuccessResponse(successResponse);
+    setFailureResponse(failureResponse);
+  };
 
 
   return (
     
     <View style={styles.container}>
-      {/* <TouchableOpacity onPress={() => naverLogin()} style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Image source={require('../assets/naver_icon.png')} />
-        </TouchableOpacity> */}
+      <TouchableOpacity onPress={() => naverLogin()} style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image source={require('../assets/drawable/naver_icon.png')} />
+        </TouchableOpacity>
     <TouchableOpacity onPress={() => kakaologin()} style={{ flexDirection: 'row', alignItems: 'center' }}>
     <Image source={require('../assets/drawable/kakao_icon.png')}  />
     
