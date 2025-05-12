cc9ea2b8fb09c6c1d456252b0fb60223a9b31a2d
rlaaudgjs2 <68803644+rlaaudgjs2@users.noreply.github.com>
2025-05-12 16:38:22 +0900

    FigmaScreen 적용

--- src/assets/styles/FigmaScreen.tsx
+++ src/assets/styles/FigmaScreen.tsx
@@ -1,23 +1,23 @@
 
 import {} from "react-native"
 import {
-  responsiveScreenHeight,
-  responsiveScreenWidth,
+  responsiveHeight,
+  responsiveWidth,
   responsiveFontSize
 }from "react-native-responsive-dimensions"
 
-// Figma 기준 사이즈: 375 x 812
+// Figma 기준 사이즈: 375 x (812 - 88) 
 const FIGMA_BASE_WIDTH = 375;
-const FIGMA_BASE_HEIGHT = 812;
+const FIGMA_BASE_HEIGHT = 724;
 
 export function widthPercentage(width : number) : number {
   const percentage = (width / FIGMA_BASE_WIDTH) * 100
-  return responsiveScreenWidth(percentage)
+  return responsiveWidth(percentage)
 }
 
 export function heightPercentage(height : number) : number {
   const percentage = (height / FIGMA_BASE_HEIGHT) * 100
-  return responsiveScreenHeight(percentage)
+  return responsiveHeight(percentage)
 }
 
 export function fontPercentage(size : number): number{
