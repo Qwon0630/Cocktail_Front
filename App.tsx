081f0502071aa62ebc1a1847144ffb21d6d1585a
rlaaudgjs2 <68803644+rlaaudgjs2@users.noreply.github.com>
2025-05-12 16:48:05 +0900

    safeAreaView

--- App.tsx
+++ App.tsx
@@ -4,9 +4,9 @@ import { StyleSheet, View, ActivityIndicator, useColorScheme } from "react-nativ
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import SplashScreen from "react-native-splash-screen";
 import Navigation from "./src/Navigation/Navigation";
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {Provider as PaperProvider} from "react-native-paper"
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 
 // import MobileAds from "react-native-google-mobile-ads";
 // import { firebase } from "@react-native-firebase/app";
@@ -56,11 +56,13 @@ function App(): React.JSX.Element {
   }
 
   return (
+    <SafeAreaProvider>
     <PaperProvider>
     <ToastProvider>
       <Navigation />
     </ToastProvider>
     </PaperProvider>
+    </SafeAreaProvider>
     );
 }
 
