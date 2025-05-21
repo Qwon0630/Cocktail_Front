dfd2ce9fecc3d493200f295b0141d0b982620163
Qwon0630 <81517768+Qwon0630@users.noreply.github.com>
2025-05-21 12:05:52 +0900

    remove-console 적용

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
+module.exports = function (api) {
+  api.cache(true);
+  const plugins = [
     'react-native-reanimated/plugin',
     ['module:react-native-dotenv', {
       moduleName: '@env',
@@ -9,6 +9,15 @@ module.exports = {
       allowlist: null,
       safe: false,
       allowUndefined: true,
-    }]
-],
+    }],
+  ];
+
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push('transform-remove-console');
+  }
+
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins,
+  };
 };
